3e50b3156102a4afb1e08f019216831f5ed2ec5f
Farkas Tamás <tamas@farkastamas.eu>
2024-05-31 10:23:35 +0200

    daisyui

--- app/components/AddToCard.tsx
+++ app/components/AddToCard.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 const AddToCard = () => {
 	return (
 		<>
-			<button onClick={() => console.log('Click')}>Add to card</button>
+			<button
+				onClick={() => console.log('Click')}
+				className='btn btn-primary'>
+				Add to card
+			</button>
 		</>
 	)
 }
