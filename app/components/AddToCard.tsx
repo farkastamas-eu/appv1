fbf73a19becde76d31d546fbee0ec649bc979f01
Farkas Tamás <tamas@farkastamas.eu>
2024-05-31 09:42:33 +0200

    Next

---
+++ app/components/AddToCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddToCard = () => {
+	return (
+		<div>
+			<button onClick={() => console.log('Click')}>Add to card</button>
+		</div>
+	)
+}
+
+export default AddToCard
---
+++ app/users/page.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './page.css'
+
+interface User {
+	id: number
+	name: string
+}
+
+const UsersPage = async () => {
+	const res = await fetch('https://jsonplaceholder.typicode.com/users', { next: { revalidate: 10 } })
+	const users: User[] = await res.json()
+	return (
+		<>
+			<h1 className='font-bold text-3xl mb-2'>Users</h1>
+			<p className='font-bold text-3xl mb-2'>{new Date().toLocaleTimeString()}</p>
+			<ul>
+				{users.map((user) => (
+					<li key={user.id}>{user.name}</li>
+				))}
+			</ul>
+		</>
+	)
+}
+
+export default UsersPage
